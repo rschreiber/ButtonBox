0516a774cd1c02d29fd5795c167a147ba8a07f05
Ryan Schreiber <ryan.schreiber@outlook.co.nz>
2025-01-08 16:13:01 +1300

    Apply various updates, change the master/slave mode

--- Leonardo/src/main.cpp
+++ Leonardo/src/main.cpp
@@ -1,43 +1,134 @@
 #include <Arduino.h>
-#include <Keyboard.h>
 #include <Wire.h>
+#include <Keyboard.h>
+
+// Define I2C slave addresses
+#define NANO_1_ADDRESS 0x04
+#define NANO_2_ADDRESS 0x05
+
+#define EMPTY_BUFFER_PLACEHOLDER 0xFF
+
+const int nanoAddresses[] = {NANO_1_ADDRESS, NANO_2_ADDRESS};
+
+// Define key mappings for each Nano
+const char keyMappings[2][16] = {
+    {'A', 'B', 'C', 'D', 'E', '-', '-', '-', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'},
+    {'N', 'O', 'P', 'Q', 'R', '-', '-', '-', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}};
+
+// Debugging
+#define DEBUG 1
+#if DEBUG
+#define DebugPrint(x) Serial.print(x)
+#define DebugPrintln(x) Serial.println(x)
+#else
+#define DebugPrint(x)
+#define DebugPrintln(x)
+#endif
 
 
-void receiveEvent(int howMany);
 
-//what to run when the processor starts up
-void setup() {
-  Serial.begin (115200);
-  delay (2000);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);   
-  Serial.println("I2C Slave Device starting up..."); 
-  //start the keyboard up
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin(); // Initialize I2C as master
   Keyboard.begin();
-  //setup i2c communication for incoming data
-  Wire.begin(0x04);
-  //tell the arduino to call the receiveEvent function when data is received
-  Wire.onReceive(receiveEvent);
-  Serial.println("I2C Slave Device Ready!");
+  delay(3000); // Delay for 3 seconds to allow Nano to start up
+  DebugPrintln("STR host controller started");
 }
 
-//what runs when a new I2C event is received
-void receiveEvent(int howMany) {
-  Serial.println("Data Received");
-  while (Wire.available()) { // Read all available bytes
-    char c = Wire.read();
-    Keyboard.write(c);      // Send the byte via Keyboard
-    Serial.print("Received byte: ");
-    Serial.println(c);
+// Function prototypes
+void pollNano(int nanoIndex);
+char mapCommandToKey(int nanoIndex, uint8_t command);
+
+bool isTimeToRun(unsigned long &lastRun, unsigned long interval)
+{
+  if (millis() - lastRun >= interval)
+  {
+    lastRun = millis();
+    return true;
+  }
+  return false;
+}
+
+
+
+void loop()
+{
+  static unsigned long lastPollTime = 0;
+  const unsigned long pollingInterval = 50;
+
+  // Poll Nanos at a regular interval
+  if (isTimeToRun(lastPollTime, pollingInterval))
+  {
+    pollNano(0);
+    pollNano(1);
+  }
+
+  // count how many loops have run and write this out every 30 seconds
+  static unsigned long lastPrintTime = 0;
+  const unsigned long printInterval = 30000;
+  static unsigned long loopCount = 0;
+  loopCount++;
+  if (isTimeToRun(lastPrintTime, printInterval))
+  {
+    DebugPrint("Loop count: ");
+    DebugPrintln(loopCount);
+    loopCount = 0;
   }
 }
 
+// Poll a Nano for commands
+void pollNano(int nanoIndex)
+{
+  int nanoAddress = nanoAddresses[nanoIndex];
+  Wire.requestFrom(nanoAddress, 1); // Request 1 byte from the Nano
 
-//what to run in the main loop
-void loop() {
-  delay(100);
+  if (Wire.available())
+  {
+
+    int command = Wire.read(); // Read the command byte
+
+    if (command != EMPTY_BUFFER_PLACEHOLDER)
+    {                                             // Ignore placeholder (empty buffer)
+      int commandType = (command & 0x80) ? 1 : 0; // Extract command type (bit 7)
+      int controllerID = command & 0x3F;          // Extract controller ID (bits 0–5)
+
+      DebugPrint("Nano ");
+      DebugPrint(nanoIndex + 1);
+      DebugPrint(": Command Type: ");
+      DebugPrint(commandType == 0 ? "PRESS" : "RELEASE");
+      DebugPrint(", Controller ID: ");
+      DebugPrintln(controllerID);
+
+      char key = mapCommandToKey(nanoIndex, controllerID);
+      if (key != 0)
+      {
+        if (commandType == 0)
+        { // PRESS
+          // Keyboard.press(key);
+          DebugPrint("Key Pressed: ");
+        }
+        else
+        { // RELEASE
+          // Keyboard.release(key);
+          DebugPrint("Key Released: ");
+        }
+        DebugPrintln(key);
+      }
+      else
+      {
+        DebugPrintln("Invalid Controller ID");
+      }
+    }
+  }
 }
 
+// Map a command to a key based on Nano and controller ID
+char mapCommandToKey(int nanoIndex, uint8_t controllerID)
+{
+  if (controllerID < 16)
+  { // Ensure valid controller ID
+    return keyMappings[nanoIndex][controllerID];
+  }
+  return 0; // Return 0 for invalid ID
+}
--- Nano/src/main.cpp
+++ Nano/src/main.cpp
@@ -25,7 +25,6 @@ unsigned long lastDebounceTime[BUTTON_COUNT] = {0, 0, 0, 0, 0};
 // #define RC2_CLK 8
 // #define RC2_DATA 9
 
-
 #define RC1_SWITCH 7
 #define RC1_CLK 8
 #define RC1_DATA 9
@@ -33,7 +32,6 @@ unsigned long lastDebounceTime[BUTTON_COUNT] = {0, 0, 0, 0, 0};
 #define RC2_SWITCH 10
 #define RC2_CLK 11
 #define RC2_DATA 12
-
 
 #define ENCODER_COUNT 2
 
@@ -67,12 +65,18 @@ int i2cAddress; // Variable to store the chosen address
 #define LED_GREEN A1
 #define LED_BLUE A2
 
-#define COMMAND_PRESS 0
-#define COMMAND_RELEASE 1
+#define MODIFIER_PRESS 0
+#define MODIFIER_RELEASE 128
 
+#define BUFFER_SIZE 16 // Size of the command buffer
+volatile uint8_t commandBuffer[BUFFER_SIZE];
+volatile uint8_t bufferHead = 0;
+volatile uint8_t bufferTail = 0;
+
+#define EMPTY_BUFFER_PLACEHOLDER 0xFF
 // Colors for modes
-int colorModeA[3] = {255, 0, 0}; // Red for Mode A
-int colorModeB[3] = {0, 0, 255}; // Blue for Mode B
+int colorModeA[3] = {255, 0, 0}; // Colour Mode A
+int colorModeB[3] = {0, 0, 255}; // Colour Mode B
 
 int modeAValues[] = {0, 1}; // Clockwise, Counterclockwise
 int modeBValues[] = {2, 3}; // Clockwise, Counterclockwise
@@ -88,6 +92,46 @@ int modeBValues[] = {2, 3}; // Clockwise, Counterclockwise
 #endif
 
 static int lastColor[3] = {255, 0, 0};
+
+void addCommand(uint8_t modifier, uint8_t command)
+{
+    // serial print details
+    DebugPrint("Modifier: ");
+    DebugPrint(modifier == MODIFIER_PRESS ? "PRESS" : "RELEASE");
+    DebugPrint(" Command: ");
+    DebugPrintln(command);
+    // Add a command to the buffer
+    uint8_t nextHead = (bufferHead + 1) % BUFFER_SIZE;
+    if (nextHead == bufferTail)
+    {
+        bufferTail = (bufferTail + 1) % BUFFER_SIZE; // Wrap around and discard the oldest command
+    }
+
+    //calculate the command value and store it in the buffer
+    uint8_t value = command | modifier;
+    commandBuffer[bufferHead] = value;
+    bufferHead = nextHead;
+}
+
+// I2C request handler
+void onRequest()
+{
+    if (bufferHead != bufferTail)
+    {
+        uint8_t command = commandBuffer[bufferTail];
+        Serial.print("Sending command: ");
+        Serial.println(command, HEX);
+        Wire.write(command);                         // Send the next command
+        bufferTail = (bufferTail + 1) % BUFFER_SIZE; // Move to the next command
+    }
+    else
+    {
+        Wire.write(EMPTY_BUFFER_PLACEHOLDER); // Placeholder for empty buffer
+    }
+}
+
+
+
 void setLedColor(int r, int g, int b)
 {
     if (lastColor[0] != r || lastColor[1] != g || lastColor[2] != b)
@@ -101,24 +145,42 @@ void setLedColor(int r, int g, int b)
     }
 }
 
+void startupFlashLed()
+{
+    //cycle through RGB colors twice to indicate startup
+    for (int i = 0; i < 2; i++)
+    {
+        setLedColor(255, 0, 0); // Red
+        delay(500);
+        setLedColor(0, 255, 0); // Green
+        delay(500);
+        setLedColor(0, 0, 255); // Blue
+        delay(500);
+    }
+}
+
 void setup()
 {
     Serial.begin(115200);
     DebugPrintln("Starting...");
 
-    //Set up the address for the I2C based on a pin state
+    // Set up the address for the I2C based on a pin state
     pinMode(ADDRESS_PIN, INPUT_PULLUP); // Configure the address pin with internal pull-up
 
     // Read the state of the address pin
-    if (digitalRead(ADDRESS_PIN) == LOW) {
+    if (digitalRead(ADDRESS_PIN) == LOW)
+    {
         i2cAddress = ADDRESS_1; // Use the first address
-    } else {
+    }
+    else
+    {
         i2cAddress = ADDRESS_2; // Use the second address
     }
 
     Serial.print("I2C address: 0x");
     Serial.println(i2cAddress, HEX);
-    Wire.begin();
+    Wire.begin(i2cAddress);    // Start I2C communication
+    Wire.onRequest(onRequest); // Register the request handler
 
     // Initialize buttons
     for (int i = 0; i < BUTTON_COUNT; i++)
@@ -144,34 +206,19 @@ void setup()
         buttonsLastState[i] = digitalRead(buttons[i]);
     }
 
+
+
     // Initialize RGB LED pins
     pinMode(LED_RED, OUTPUT);
     pinMode(LED_GREEN, OUTPUT);
     pinMode(LED_BLUE, OUTPUT);
 
+    startupFlashLed();
+
     // Set initial color based on Encoder 1 mode
     setLedColor(colorModeA[0], colorModeA[1], colorModeA[2]);
 
     DebugPrintln("Started.");
-}
-
-void sendCommand(int commandType, int controller)
-{
-
-    if (commandType == COMMAND_PRESS)
-    {
-        DebugPrint("Button pressed: ");
-    }
-    else
-    {
-        DebugPrint("Button released: ");
-    }
-    DebugPrintln(controller);
-
-    Wire.beginTransmission(i2cAddress);
-    Wire.write(commandType);
-    Wire.write(controller);
-    Wire.endTransmission();
 }
 
 void handleEncoderSwitch(int encoder)
@@ -192,7 +239,6 @@ void handleEncoderSwitch(int encoder)
 
                 if (checkState == BUTTON_PRESSED)
                 {
-
                     encoderMode[encoder] = !encoderMode[encoder]; // Toggle mode
 
                     // Change LED color based on mode
@@ -266,18 +312,18 @@ void handleEncoderRotation(int encoder)
 
         if (isReleased[encoder]) // Only send a "press" if "release" was sent
         {
-            sendCommand(COMMAND_PRESS, value); // Send "press" event
-            isReleased[encoder] = false; // Mark as "pressed"
-            lastValue[encoder] = value;  // Store the value for the release event
+            addCommand(MODIFIER_PRESS, value); // Send "press" event
+            isReleased[encoder] = false;       // Mark as "pressed"
+            lastValue[encoder] = value;        // Store the value for the release event
         }
     }
 
     // Handle non-blocking "release" event
     if (!isReleased[encoder] && millis() - lastReleaseTime[encoder] >= ENCODER_DELAY)
     {
-        sendCommand(COMMAND_RELEASE, lastValue[encoder]); // Send "release" event with the stored value
-        lastReleaseTime[encoder] = millis(); // Update the last release time
-        isReleased[encoder] = true;          // Mark as "released"
+        addCommand(MODIFIER_RELEASE, lastValue[encoder]); // Send "release" event with the stored value
+        lastReleaseTime[encoder] = millis();              // Update the last release time
+        isReleased[encoder] = true;                       // Mark as "released"
     }
 
     lastCLKState[encoder] = currentCLKState; // Update the last state
@@ -301,11 +347,11 @@ void checkButtons()
 
                 if (buttonsState[i] == BUTTON_PRESSED)
                 {
-                    sendCommand(COMMAND_PRESS, i);
+                    addCommand(MODIFIER_PRESS, i);
                 }
                 else
                 {
-                    sendCommand(COMMAND_RELEASE, i);
+                    addCommand(MODIFIER_RELEASE, i);
                 }
             }
         }
